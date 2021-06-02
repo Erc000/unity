d9ed764d0c4dc6157c486c299646258400cce526
Robert-Neville <66701111+Robert-Neville@users.noreply.github.com>
2021-06-02 12:36:22 -0400

    Add files via upload

---
+++ Assets/rubik.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rubik : MonoBehaviour {
+	
+	public GameObject up,down,right,left,front,back;
+	public int invert;
+
+	// Use this for initialization
+	void Start () {
+		invert = 90;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		invert = 90;
+		if(Input.GetKeyDown(KeyCode.LeftShift)){
+			invert = -90;
+		}
+
+		if(Input.GetKeyDown(KeyCode.W)){
+			Instantiate(up,up.transform.position,up.transform.rotation);
+		}
+		if(Input.GetKeyDown(KeyCode.S)){
+			Instantiate(down,down.transform.position,down.transform.rotation);
+		}
+		if(Input.GetKeyDown(KeyCode.D)){
+			Instantiate(right,right.transform.position,right.transform.rotation);
+		}
+		if(Input.GetKeyDown(KeyCode.A)){
+			Instantiate(left,left.transform.position,left.transform.rotation);
+		}
+		if(Input.GetKeyDown(KeyCode.E)){
+			Instantiate(front,front.transform.position,front.transform.rotation);
+		}
+		if(Input.GetKeyDown(KeyCode.Q)){
+			Instantiate(back,back.transform.position,back.transform.rotation);
+		}
+
+	}
+}
---
+++ Assets/up.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class up : MonoBehaviour {
+	public Transform ypos;
+	GameObject u1;
+	public int invert =90;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		invert = GetComponent<rubik> ().invert;
+		Destroy(gameObject);
+	}
+	private void OnCollisionEnter(Collision collision){
+		u1 = collision.gameObject;
+
+		u1.transform.RotateAround(ypos.position,transform.up,invert);
+	}
+}
